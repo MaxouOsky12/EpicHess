a3877117b30805acb4a78c0dc2cd41862c5c0aba
MaxouOsky12 <146654082+MaxouOsky12@users.noreply.github.com>
2025-01-29 15:57:55 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+// Compteur de visiteurs
+let visitorCount = localStorage.getItem('visitorCount') || 0;
+visitorCount++;
+localStorage.setItem('visitorCount', visitorCount);
+
+// Met à jour l'affichage du compteur de visiteurs
+document.addEventListener('DOMContentLoaded', function() {
+    const visitorElement = document.getElementById('visitor-count');
+    if (visitorElement) {
+        visitorElement.innerText = `Visiteurs : ${visitorCount}`;
+    }
+});
+
+// Compteur de joueurs testant les maps
 document.addEventListener('DOMContentLoaded', function () {
     let playersCount = localStorage.getItem('playersCount') || 0;
     document.getElementById('players-count').innerText = playersCount;
@@ -7,7 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
             playersCount++;
             localStorage.setItem('playersCount', playersCount);
             document.getElementById('players-count').innerText = playersCount;
-
             alert("Merci d'avoir testé notre map !");
         });
     });
